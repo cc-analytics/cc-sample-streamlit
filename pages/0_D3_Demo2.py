9c344513ceccb4da2511f7f77ceeee023e39c10a
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 12:52:33 -0800

    Changed iframe size and allow scrolling.

--- pages/0_D3_Demo2.py
+++ pages/0_D3_Demo2.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 st.title("Online Sales in D3")
 st.markdown(" Created with D3 and hosted on my Google Cloud")
-components.iframe("https://tinyurl.com/4zv2fhc6")
+components.iframe("https://tinyurl.com/4zv2fhc6", width = '800', height = '600', scrolling = True)
