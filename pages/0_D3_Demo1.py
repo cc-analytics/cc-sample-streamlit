7d1aafdcafdccf9e8048af4dcfe10f8ccbd9d05a
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 12:54:57 -0800

    Changed iframe sizes.

--- pages/0_D3_Demo1.py
+++ pages/0_D3_Demo1.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 st.title("Retention example in D3")
 st.markdown(" Created with D3 and hosted on my Google Cloud")
-components.iframe("https://tinyurl.com/48kwcs4r", width = 800, height = 600, scrolling = True)
+components.iframe("https://tinyurl.com/48kwcs4r", width = 1000, height = 800, scrolling = True)
