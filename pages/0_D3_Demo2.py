ab532fdbe6b05a8f261dabb6182e6ef0c76d0020
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 13:13:28 -0800

    Added URL of D3 demo.

--- pages/0_D3_Demo2.py
+++ pages/0_D3_Demo2.py
@@ -3,7 +3,8 @@ import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
 st.title("Online Sales in D3")
-st.markdown(" Created with D3 and hosted on my Google Cloud")
+st.markdown("# Created with D3 and hosted on my Google Cloud")
+st.markdown("url: https://tinyurl.com/48kwcs4r")
 components.iframe("https://tinyurl.com/4zv2fhc6", width = 1000, height = 800, scrolling = True)
 st.sidebar.title("Chris Chen")
 st.sidebar.markdown("# Seasoned Data Analytics Professional")
