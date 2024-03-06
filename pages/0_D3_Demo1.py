ce0612d843276d1d0a06a6ca3af2897a986832ff
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 13:17:34 -0800

    Changed font sizes.

--- pages/0_D3_Demo1.py
+++ pages/0_D3_Demo1.py
@@ -3,8 +3,8 @@ import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
 st.title("Retention example in D3")
-st.markdown("# Created with D3 and hosted on my Google Cloud")
-st.markdown(" url: https://tinyurl.com/48kwcs4r")
+st.markdown("#### Created with D3 and hosted on my Google Cloud")
+st.markdown("##### url: https://tinyurl.com/48kwcs4r")
 components.iframe("https://tinyurl.com/48kwcs4r", width = 1200, height = 760, scrolling = True)
 st.sidebar.title("Chris Chen")
 st.sidebar.markdown("# Seasoned Data Analytics Professional")
