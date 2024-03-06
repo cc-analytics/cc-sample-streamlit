080a93ea476f02163d5544122db563ebdc27e2d0
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 12:50:01 -0800

    Changed font sizes.

--- pages/0_D3_Demo2.py
+++ pages/0_D3_Demo2.py
@@ -2,5 +2,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # embed streamlit docs in a streamlit app
-st.title("Online Sales in D3, hosted on my Google Cloud")
+st.title("Online Sales in D3")
+st.markdown(" Created with D3 and hosted on my Google Cloud")
 components.iframe("https://tinyurl.com/4zv2fhc6")
