60ccb4373f6791ab7ef677140e4f16a5969a4bd5
Chris Chen <chrischen.analytics@gmail.com>
2024-03-06 13:09:24 -0800

    Changed sidebar of pages.

--- pages/0_D3_Demo1.py
+++ pages/0_D3_Demo1.py
@@ -4,4 +4,8 @@ import streamlit.components.v1 as components
 # embed streamlit docs in a streamlit app
 st.title("Retention example in D3")
 st.markdown(" Created with D3 and hosted on my Google Cloud")
-components.iframe("https://tinyurl.com/48kwcs4r", width = 1000, height = 800, scrolling = True)
+components.iframe("https://tinyurl.com/48kwcs4r", width = 1200, height = 760, scrolling = True)
+st.sidebar.title("Chris Chen")
+st.sidebar.markdown("# Seasoned Data Analytics Professional")
+st.sidebar.markdown("chrischen.analytics@gmail.com")
+st.sidebar.markdown("https://www.linkedin.com/in/chrischenanalytics")
--- pages/0_D3_Demo2.py
+++ pages/0_D3_Demo2.py
@@ -5,3 +5,7 @@ import streamlit.components.v1 as components
 st.title("Online Sales in D3")
 st.markdown(" Created with D3 and hosted on my Google Cloud")
 components.iframe("https://tinyurl.com/4zv2fhc6", width = 1000, height = 800, scrolling = True)
+st.sidebar.title("Chris Chen")
+st.sidebar.markdown("# Seasoned Data Analytics Professional")
+st.sidebar.markdown("chrischen.analytics@gmail.com")
+st.sidebar.markdown("https://www.linkedin.com/in/chrischenanalytics")
