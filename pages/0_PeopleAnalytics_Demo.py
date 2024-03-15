8fc1affce55b41018b4dcd6542ec5f242d2d8151
Chris Chen <chrischen.analytics@gmail.com>
2024-03-14 17:28:23 -0700

    Cleaned up code; added more charts; added dataframe display;

--- pages/0_PeopleAnalytics_Demo.py
+++ pages/0_PeopleAnalytics_Demo.py
@@ -1,188 +1,218 @@
 import streamlit as st
-import streamlit.components.v1 as components
-from datetime import datetime, timedelta, date
-from streamlit_pills import pills
-# ----------------------Importing utils.py----------------------
-import io
-from utils import (
-    init_connection,
-    run_query,
-    show_code
-)
-
-from urllib.error import URLError
-
-import altair as alt
+from datetime import datetime
 import pandas as pd
+import altair as alt
+from streamlit_pills import pills
 
+# Utils imports
+from utils import run_query, init_connection  
+
+# Set Streamlit page configuration at the beginning
+st.set_page_config(page_title="People Analytics Demo", page_icon="🧑", layout="wide")
+
+# Decorator for caching data loading function
 @st.cache_data
 def load_people_data():
-    results = run_query("SELECT A.EMPLOYEE_ID, A.NAME, A.BIRTH_DATE, A.\"Education\", A.HIRED_DATE, A.JOB_NAME, A.DEPARTMENT_NAME, B.\"Division\" as DIVISION_NAME from CCMOCKUP.PUBLIC.EMPLOYEE_TEST A join CCMOCKUP.PUBLIC.DEPARTMENT_TEST B on A.DEPARTMENT_NAME = B.\"Department\"")
-    return results
+    # query = "SELECT A.EMPLOYEE_ID, A.NAME, A.BIRTH_DATE, A.\"Education\" as EDUCATION, A.HIRED_DATE, A.JOB_NAME, A.DEPARTMENT_NAME, B.\"Division\" as DIVISION_NAME from CCMOCKUP.PUBLIC.EMPLOYEE_TEST A join CCMOCKUP.PUBLIC.DEPARTMENT_TEST B on A.DEPARTMENT_NAME = B.\"Department\""
+    query = "SELECT A.EMPLOYEE_ID, A.NAME, A.BIRTH_DATE, A.\"Education\" as EDUCATION, A.HIRED_DATE, A.JOB_NAME, A.DEPARTMENT_NAME, B.\"Division\" as DIVISION_NAME, DATEADD(DAY, C.EMPLOYED_LENGTH, A.HIRED_DATE) as END_DATE from CCMOCKUP.PUBLIC.EMPLOYEE_TEST A join CCMOCKUP.PUBLIC.DEPARTMENT_TEST B on A.DEPARTMENT_NAME = B.\"Department\" left join CCMOCKUP.PUBLIC.EMPLOYED_LENGTH_TEST C on A.EMPLOYEE_ID = C.EMPLOYEE_ID"
+    return run_query(query)
 
 def calculate_age(birth_date):
     today = datetime.now()
     age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
     return age
 
-st.set_page_config(page_title="People Analytics Demo", page_icon="🧑", layout="wide")
-st.title("People Analytics Demo")
-st.sidebar.markdown("##### Created by:")
-st.sidebar.markdown("# Chris Chen")
-st.sidebar.markdown("## Seasoned Data Analytics Professional")
-st.sidebar.markdown("chrischen.analytics@gmail.com")
-st.sidebar.markdown("https://www.linkedin.com/in/chrischenanalytics")
+def main():
+    st.title("People Analytics Demo")
+    # Sidebar
+    st.sidebar.markdown("##### Created by: Chris Chen\n## Seasoned Data Analytics Professional\nchrischen.analytics@gmail.com\nhttps://www.linkedin.com/in/chrischenanalytics")
 
-tabMain, tabInfo, tabTo_dos = st.tabs(["Main", "Info", "To-do's"])
+    # Tabs
+    tab_main, tab_info, tab_to_dos = st.tabs(["Main", "Info", "To-do's"])
 
-with tabMain:
-
-    st.write("")
-
-    c30, c31, c32 = st.columns([0.2, 0.1, 3])
-
-    st.subheader("Select Topic:")
-
-    example = pills(
+    with tab_main:
+        topic = pills(
         "",
         [
-            "Overview",
-            "Employee Retention",
-            "Recruitment"
+            "Trend",
+            "Headcount",
+            "Recruitment",
+            "Dataframe"
         ],
         [
-            "📊",
             "📈",
-            "💼"
+            "📊",
+            "💼",
+            "📃"
         ],
         label_visibility="collapsed"
-    )
-    submitted = st.button("Run!")
-    if submitted:
+        )
+        # if st.button("Run!"):
         df = load_people_data()
-        if example == "Overview":
-            # Generating the count of records with 'hired_date' greater than each date
-            st.markdown("### Total Headcount: ")
-            total_Count = df['HIRED_DATE'].count()
-            st.write(total_Count)            
-            counts = df['HIRED_DATE'].value_counts().sort_index().cumsum()[::-1]
-            counts_df = pd.DataFrame({
-                'Hired Date': counts.index,
-                'Cumulative Count': counts.values
-            })
-            # Group by 'HIRED_DATE' and count occurrences
-            hiring_counts_df = df.groupby('HIRED_DATE')['HIRED_DATE'].count().reset_index(name='Count')
+        if topic == "Trend":
+            show_trend(df)
+        elif topic == "Headcount":
+            show_headcount(df)
+        elif topic == "Recruitment":
+            show_recruitment(df)
+        elif topic == "Dataframe":
+            show_dataframe(df)
 
-            # Renaming the 'Value' column to 'Count' to better reflect the new content
-            hiring_counts_df = hiring_counts_df.rename(columns={'Value': 'Count'})
-
-            row1 =  st.columns(2, gap="small")
-            with row1[0]:
-                st.write("Cumulative Head Count")
-                chart = st.line_chart(counts_df.set_index('Hired Date'),width=200, height=260)
-            with row1[1]:
-                st.write("Historical Hiring")
-                chart = st.line_chart(hiring_counts_df.set_index('HIRED_DATE'),width=200, height=260)
-            row2 = st.columns(2, gap="small")
-            with row2[0]:
-                st.write("Head Count by Division")
-                counts_by_division_df = df.groupby('DIVISION_NAME')['DIVISION_NAME'].count().reset_index(name='Count')
-                bar_chart = st.bar_chart(data=counts_by_division_df,x='DIVISION_NAME')
-            with row2[1]:
-                st.write("Head Count by Department")
-                counts_by_division_df = df.groupby('DEPARTMENT_NAME')['DEPARTMENT_NAME'].count().reset_index(name='Count')
-                bar_chart = st.bar_chart(data=counts_by_division_df,x='DEPARTMENT_NAME')
-                # st.dataframe(counts_by_division_df.sort_values(by='Count', ascending=False))
-            row3 = st.columns(2, gap="small")
-            with row3[0]:
-                st.write("Head Count by Age")
-                df['AGE'] = df['BIRTH_DATE'].apply(calculate_age)
-                counts_by_age_df = df.groupby("AGE")["AGE"].count().reset_index(name="Count")
-                bar_chart = st.bar_chart(data=counts_by_age_df,x='AGE')                
-            # with row3[1]:
-
-with tabInfo:
-    st.write("")
-    st.write("")
-    st.subheader(" Streamlit App Demo")    
-    st.markdown(
+    with tab_info:
+        st.markdown(
     """This Streamlit app connects to my Snowflake data tables.  The mock-up values are created by OpenAI's GPT-4.  Cache is implemented so it does not hit the tables every time (to save my wallet 😄).
     Then it does the calculations in Python and display the charts.  It shows how you can use Streamlit to build a dashboard.
     I will include my sample app in Tableau, too."""
 )
-    st.write("")
-    st.subheader(" What is People Analytics?")
-    st.markdown(
-        """People Analytics is the data-driven approach to managing people at work. By leveraging data analysis, statistics, and technology, it aims to improve decision-making, enhance employee experience, and boost organizational performance. It covers areas like recruitment, performance evaluation, leadership development, and employee retention, turning insights into actionable strategies.
-"""
-    )
-    st.write("")
-    st.subheader("🎈 What is Streamlit?")
-    st.markdown(
-        "[Streamlit](https://streamlit.io) is an open-source Python library that allows users to create interactive, web-based data visualization and machine learning applications without the need for extensive web development knowledge"
-    )
-
-    st.write("---")
-
-    st.subheader("📖 Resources")
-    st.markdown(
-        """
-    - OpenAI
-        - [OpenAI Playground](https://beta.openai.com/playground)
-        - [OpenAI Documentation](https://beta.openai.com/docs)    
-    - Streamlit
-        - [Documentation](https://docs.streamlit.io/)
-        - [Gallery](https://streamlit.io/gallery)
-        - [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
-        - [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
-        - Deploy your apps using [Streamlit Community Cloud](https://streamlit.io/cloud) in just a few clicks 
-    """)
-
-with tabTo_dos:
-    with st.expander("To-do", expanded=True):
-        st.write(
-            """
-        - Deploy the chart component
-        - Add a link to the Tableau version
-        """
+        st.write("")
+        st.subheader(" What is People Analytics?")
+        st.markdown(
+            """People Analytics is the data-driven approach to managing people at work. By leveraging data analysis, statistics, and technology, it aims to improve decision-making, enhance employee experience, and boost organizational performance. It covers areas like recruitment, performance evaluation, leadership development, and employee retention, turning insights into actionable strategies.
+    """
         )
         st.write("")
-    with st.expander("Done", expanded=True):
-        st.write(
-            """
-        - Include information in the 'Info' tab.
-        - Set up app layout.
-        - Create Snowflake tables.
-        - Populate mock-up data gnerated by GPT-4.
-        - Create People Analytics metrics.
-
-        """
+        st.subheader("🎈 What is Streamlit?")
+        st.markdown(
+            "[Streamlit](https://streamlit.io) is an open-source Python library that allows users to create interactive, web-based data visualization and machine learning applications without the need for extensive web development knowledge"
         )
-        st.write("")
 
+        st.write("---")
 
-# conn = init_connection()
-    
+        st.subheader("📖 Resources")
+        st.markdown(
+            """
+        - OpenAI
+            - [OpenAI Playground](https://beta.openai.com/playground)
+            - [OpenAI Documentation](https://beta.openai.com/docs)    
+        - Streamlit
+            - [Documentation](https://docs.streamlit.io/)
+            - [Gallery](https://streamlit.io/gallery)
+            - [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
+            - [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
+            - Deploy your apps using [Streamlit Community Cloud](https://streamlit.io/cloud) in just a few clicks 
+        """)
 
+    with tab_to_dos:
+        with st.expander("To-do", expanded=True):
+            st.write(
+                """
+            - Add a link to the Tableau version
+            - Add more metrics
+            """
+            )
+            st.write("")
+        with st.expander("Done", expanded=True):
+            st.write(
+                """
+            - Deploy the chart component
+            - Include information in the 'Info' tab.
+            - Set up app layout.
+            - Create Snowflake tables.
+            - Populate mock-up data gnerated by GPT-4.
+            - Create People Analytics metrics.
 
-# def load_segment():
-#     results = run_query("SELECT A.C_MKTSEGMENT as Segment, SUM(A.C_ACCTBAL) as Balance from CUSTOMER A  group by  A.C_MKTSEGMENT ")
-#     # return results.set_index("N_NAME")
-#     return results
-# df = None
-# # try:
+            """
+            )
 
-# with st.container():
-#     bt1 = st.button("Show Acct Balance by Country, Segment")
-#     bt2 = st.button("Show Acct Balance by Segment")
+def show_trend(df):
+    trend_layout(df)
 
-# with st.container():
-#     row = st.columns(2)
-#     if bt1:
-#         df = load_country_segment()
-#         st.dataframe(df)
-#     elif bt2:
-#         df = load_segment()    
-#         row[0].bar_chart(df, x="SEGMENT", y="BALANCE")
-#         row[1].dataframe(df)
+def show_headcount(df):
+    headcount_layout(df)
 
+def show_recruitment(df):
+    recruitment_layout(df)
+
+def show_dataframe(df):
+    dataframe_layout(df)    
+
+# Layout
+def trend_layout(df):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("### Headcount Over Time")
+        show_cumulative_headcount(df)
+    # with col2:
+        # st.markdown("### Headcount by Division")
+        # show_headcount_by_division(df)
+        
+def headcount_layout(df):
+    r1col1, r1col2 = st.columns(2)
+    with r1col1:
+        st.markdown("### Headcount by Division")
+        show_headcount_by_division(df)
+    with r1col2:
+        st.markdown("### Headcount by Department")
+        show_headcount_by_department(df)        
+    r2col1, r2col2 = st.columns(2)
+    with r2col1:
+        st.markdown("### Headcount by Age")
+        show_headcount_by_age(df)
+    with r2col2:
+        st.markdown("### Headcount by Education")
+        show_headcount_by_education(df)
+        
+def recruitment_layout(df):
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("### Historical Hirings")
+        show_hirings(df)
+    with col2:
+        st.markdown("### Historical Departure")
+        show_departures(df)
+
+def dataframe_layout(df):
+    col1, col2 = st.columns(2)
+    with col1:
+        show_dataframe(df)    
+
+# Actual implementation
+def show_cumulative_headcount(df):
+    df = df
+
+    df['HIRED_DATE'] = pd.to_datetime(df['HIRED_DATE'])
+    df['END_DATE'] = pd.to_datetime(df['END_DATE'])
+
+    # # Generate date range
+    date_range = pd.date_range(start=df['HIRED_DATE'].min(), end=datetime(2024,3,1))
+    # # Initialize a Series to hold the count for each date
+    date_counts = pd.Series(index=date_range, data='DATE')
+    # Count qualifying records for each date
+    for single_date in date_range:
+        date_counts[single_date] = ((df['HIRED_DATE'] <= single_date) &( (df['END_DATE'] >= single_date) | pd.isna(df['END_DATE']))).sum()
+    chart = st.line_chart(date_counts, width=200, height=260)
+
+def show_headcount_by_division(df):
+    counts_by_division_df = df.groupby('DIVISION_NAME')['DIVISION_NAME'].count().reset_index(name='Count')
+    bar_chart = st.bar_chart(data=counts_by_division_df,x='DIVISION_NAME')
+
+def show_headcount_by_department(df):
+    counts_by_department_df = df.groupby('DEPARTMENT_NAME')['DEPARTMENT_NAME'].count().reset_index(name='Count')
+    bar_chart = st.bar_chart(data=counts_by_department_df,x='DEPARTMENT_NAME')
+
+def show_headcount_by_age(df):
+    df['AGE'] = df['BIRTH_DATE'].apply(calculate_age)
+    counts_by_age_df = df.groupby('AGE')['AGE'].count().reset_index(name='Count')
+    bar_chart = st.bar_chart(data=counts_by_age_df,x='AGE')
+
+def show_headcount_by_education(df):
+    counts_by_education_df = df.groupby('EDUCATION')['EDUCATION'].count().reset_index(name='Count')
+    bar_chart = st.bar_chart(data=counts_by_education_df,x='EDUCATION')   
+
+def show_hirings(df):
+    hiring_counts_df = df.groupby('HIRED_DATE')['HIRED_DATE'].count().reset_index(name='Count')    
+    # chart = st.line_chart(hiring_counts_df.set_index('HIRED_DATE'),width=200, height=260)
+    chart = st.line_chart(hiring_counts_df, x="HIRED_DATE", y="Count", width=200, height=260)
+
+def show_departures(df):
+    df['END_DATE'] = pd.to_datetime(df['END_DATE'])
+    df = df[df['END_DATE'] < datetime.today()]
+    departures_counts_df = df.groupby('END_DATE')['END_DATE'].count().reset_index(name='Count')    
+    chart = st.line_chart(departures_counts_df, x="END_DATE", y="Count", width=200, height=260)    
+
+def show_dataframe(df):
+    st.markdown("###### Dataframe created from Snowflake Database")
+    st.dataframe(df)   
+
+if __name__ == "__main__":
+    main()
